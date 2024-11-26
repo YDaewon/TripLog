aafa59b28290bb79f9a3a4e041a02f2dafc030a2
김휘동 <rlagnlehd0417@gmail.com>
2024-11-26 10:46:40 +0900

    update

--- Triplog/bin/src/main/resources/static/assets/js/key.js
+++
@@ -1,2 +0,0 @@
-const serviceKey = "diC6PyYdpVGPE8uhOl5imJyQPLidyzEO2pI%2FcTHtSEJiNVUuVZrEHSswUYBNVc5gX1EN4JEmjU9hheK%2BhRmTaw%3D%3D";
-// const kakaoKey = "----- 발급받은 KAKAO API KEY를 입력하세요 -----";
--- Triplog/src/main/java/com/triplog/file/service/FileUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.triplog.file.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import io.jsonwebtoken.io.IOException;
-
-public interface FileUploadService {
-	public String upload(String articleNo, MultipartFile uploadFile) throws IOException, IllegalStateException, java.io.IOException;
-}
